286395445a69a595c1f320cddda30abc4df58ae9
Mitchell-Liu <32404146+Mitchell-Liu@users.noreply.github.com>
2022-12-22 19:07:52 -0800

    Create 5.Longest_Palindromic_Substring.py

---
+++ 5.Longest_Palindromic_Substring.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        max_len = 0
+        max_p = ""
+        for i in range(len(s)):
+            for j in range(i, i + 2):
+                b = i
+                e = j
+                while b >= 0 and e < len(s) and s[b] == s[e]:
+                    b -= 1
+                    e += 1
+                l = e - b - 1
+                if l > max_len:
+                    max_len = l
+                    max_p = s[b + 1 : e]
+        return max_p
